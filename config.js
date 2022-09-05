8668c7ab5c97ab55e9cb087fac443f26aa79483e
Matteo-Balboni <107464657+Matteo-Balboni@users.noreply.github.com>
2022-09-05 11:32:34 +0200

    Started ignoring config.js for real this time

--- config.js
+++
@@ -1,5 +0,0 @@
-const username = 'admin'; //string, sets the username for accessing CouchDB
-const password = 'Admin'; //string, sets the password for accessing CouchDB
-const credentials = username + ':' + password; //packs the credentials ready for use, you should not change this
-const serverPort = 81; //number, sets the server port
-module.exports = { couchCreds: credentials, serverPort: serverPort }; //exports the configuration for use, you should also not change this
