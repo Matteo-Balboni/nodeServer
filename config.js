346c582bd24e888eb43e5a8d030d6a0612f187cf
Matteo-Balboni <107464657+Matteo-Balboni@users.noreply.github.com>
2022-09-05 11:28:14 +0200

    don't mind this commit, just testing a thing

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const username = 'Admin'; //string, sets the username for accessing CouchDB
+const username = 'admin'; //string, sets the username for accessing CouchDB
 const password = 'Admin'; //string, sets the password for accessing CouchDB
 const credentials = username + ':' + password; //packs the credentials ready for use, you should not change this
-const serverPort = 80; //number, sets the server port
+const serverPort = 81; //number, sets the server port
 module.exports = { couchCreds: credentials, serverPort: serverPort }; //exports the configuration for use, you should also not change this
