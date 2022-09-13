6ac29358555a7d5692c81b9bc54e175948d3fd13
Matteo-Balboni <107464657+Matteo-Balboni@users.noreply.github.com>
2022-09-13 15:39:32 +0200

    fixed auth.js not using config file parameters

--- files/middleware/auth.js
+++ files/middleware/auth.js
@@ -1,5 +1,6 @@
+const config = require('../../config/config.js');
 const jwt = require('jsonwebtoken');
-const jwtSecret = '9e65be37f950cbd11d3d506bbf2207ba659e776913cd913e8f534751920aa3f146e0ee';  //in teoria dicono che è sicuro lasciarla qua così, ma in caso si vedrà
+const jwtSecret = config.jwtSecret;  //in teoria dicono che è sicuro lasciarla qua così, ma in caso si vedrà
 
 exports.adminAuth = async function(req, res, next) {
   const token = req.cookies.jwt;
